b8d267d9fd5ca2f1643544d078a12e4a2b5fd454
jimthecactus <jimthecactus@gmail.com>
2018-06-24 14:33:28 -0600

    Added HUD Elements and Light Flicker
    Former-commit-id: bccc5cd43dcbab852b36cd534cd2900da659d58a

---
+++ BladeRush/Assets/Scripts/VFX Tools/FlameWiggle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameWiggle : MonoBehaviour {
+
+    public float Wiggle_Limit=0.5f;
+    public float Wiggle_Amount=0.05f;
+
+    private Vector3 ocurrentoffset;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 shift = new Vector3(Random.Range(-Wiggle_Amount, Wiggle_Amount), Random.Range(-Wiggle_Amount, Wiggle_Amount), Random.Range(-Wiggle_Amount, Wiggle_Amount));
+        ocurrentoffset += shift;
+        ocurrentoffset = Vector3.ClampMagnitude(ocurrentoffset, Wiggle_Limit);
+        gameObject.transform.localPosition=ocurrentoffset;
+    }
+}
