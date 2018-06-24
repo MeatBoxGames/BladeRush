f912e13c3fa5dd3c710b6141ec9273361d95178e
jimthecactus <jimthecactus@gmail.com>
2018-06-24 00:49:04 -0600

    Added Victory Mechanics
    Former-commit-id: 0bb55127a3946c62be8fb014f18f5cf4eca970b0

---
+++ BladeRush/Assets/Scripts/Gamemode/GameMode.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void EnemyDied(Enemy deadenemy)
+    {
+
+    }
+}
